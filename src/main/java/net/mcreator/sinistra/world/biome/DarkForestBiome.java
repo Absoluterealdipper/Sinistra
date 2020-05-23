90aab6cd5f20ca3cadd961b7424967644fbfaf71
Draconis Lupos <Dragonwolf.origin@gmail.com>
2020-05-23 20:56:27 +0200

    Did some shit hope ya like it

--- src/main/java/net/mcreator/sinistra/world/biome/DarkForestBiome.java
+++ src/main/java/net/mcreator/sinistra/world/biome/DarkForestBiome.java
@@ -12,17 +12,32 @@ import net.minecraft.world.gen.surfacebuilders.SurfaceBuilder;
 import net.minecraft.world.gen.placement.Placement;
 import net.minecraft.world.gen.placement.FrequencyConfig;
 import net.minecraft.world.gen.placement.AtSurfaceWithExtraConfig;
-import net.minecraft.world.gen.feature.MultipleRandomFeatureConfig;
 import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.BaseTreeFeatureConfig;
+import net.minecraft.world.gen.feature.AbstractTreeFeature;
+import net.minecraft.world.gen.blockstateprovider.SimpleBlockStateProvider;
+import net.minecraft.world.gen.IWorldGenerationReader;
 import net.minecraft.world.gen.GenerationStage;
 import net.minecraft.world.biome.DefaultBiomeFeatures;
 import net.minecraft.world.biome.Biome;
+import net.minecraft.world.IWorldWriter;
+import net.minecraft.world.IWorld;
+import net.minecraft.util.math.MutableBoundingBox;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.Direction;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
 
+import net.mcreator.sinistra.block.DarkOAKlogBlock;
 import net.mcreator.sinistra.block.DarkGrassBlockBlock;
 import net.mcreator.sinistra.block.DarkDirtBlock;
 import net.mcreator.sinistra.SinistraModElements;
 
-import com.google.common.collect.ImmutableList;
+import java.util.Set;
+import java.util.Random;
 
 @SinistraModElements.ModElement.Tag
 public class DarkForestBiome extends SinistraModElements.ModElement {
@@ -56,13 +71,11 @@ public class DarkForestBiome extends SinistraModElements.ModElement {
 					.withPlacement(Placement.COUNT_HEIGHTMAP_32.configure(new FrequencyConfig(4))));
 			addFeature(GenerationStage.Decoration.VEGETAL_DECORATION, Feature.RANDOM_PATCH.withConfiguration(DefaultBiomeFeatures.GRASS_CONFIG)
 					.withPlacement(Placement.COUNT_HEIGHTMAP_DOUBLE.configure(new FrequencyConfig(4))));
-			addFeature(GenerationStage.Decoration.VEGETAL_DECORATION,
-					Feature.RANDOM_SELECTOR
-							.withConfiguration(new MultipleRandomFeatureConfig(
-									ImmutableList.of(Feature.NORMAL_TREE.withConfiguration(DefaultBiomeFeatures.field_230129_h_).withChance(0.2F),
-											Feature.FANCY_TREE.withConfiguration(DefaultBiomeFeatures.field_230131_m_).withChance(0.1F)),
-									Feature.NORMAL_TREE.withConfiguration(DefaultBiomeFeatures.field_230132_o_)))
-							.withPlacement(Placement.COUNT_EXTRA_HEIGHTMAP.configure(new AtSurfaceWithExtraConfig(6, 0.1F, 1))));
+			addFeature(GenerationStage.Decoration.VEGETAL_DECORATION, new CustomTreeFeature()
+					.withConfiguration((new BaseTreeFeatureConfig.Builder(new SimpleBlockStateProvider(DarkOAKlogBlock.block.getDefaultState()),
+							new SimpleBlockStateProvider(Blocks.OAK_LEAVES.getDefaultState()))).baseHeight(8)
+									.setSapling((net.minecraftforge.common.IPlantable) Blocks.JUNGLE_SAPLING).build())
+					.withPlacement(Placement.COUNT_EXTRA_HEIGHTMAP.configure(new AtSurfaceWithExtraConfig(6, 0.1F, 1))));
 		}
 
 		@OnlyIn(Dist.CLIENT)
@@ -81,6 +94,126 @@ public class DarkForestBiome extends SinistraModElements.ModElement {
 		@Override
 		public int getSkyColor() {
 			return -13421773;
+		}
+	}
+
+	static class CustomTreeFeature extends AbstractTreeFeature<BaseTreeFeatureConfig> {
+		CustomTreeFeature() {
+			super(BaseTreeFeatureConfig::deserialize);
+		}
+
+		@Override
+		protected boolean place(IWorldGenerationReader worldgen, Random rand, BlockPos position, Set<BlockPos> changedBlocks,
+				Set<BlockPos> changedBlocks2, MutableBoundingBox bbox, BaseTreeFeatureConfig conf) {
+			if (!(worldgen instanceof IWorld))
+				return false;
+			IWorld world = (IWorld) worldgen;
+			int height = rand.nextInt(5) + 8;
+			boolean spawnTree = true;
+			if (position.getY() >= 1 && position.getY() + height + 1 <= world.getHeight()) {
+				for (int j = position.getY(); j <= position.getY() + 1 + height; j++) {
+					int k = 1;
+					if (j == position.getY())
+						k = 0;
+					if (j >= position.getY() + height - 1)
+						k = 2;
+					for (int px = position.getX() - k; px <= position.getX() + k && spawnTree; px++) {
+						for (int pz = position.getZ() - k; pz <= position.getZ() + k && spawnTree; pz++) {
+							if (j >= 0 && j < world.getHeight()) {
+								if (!this.isReplaceable(world, new BlockPos(px, j, pz))) {
+									spawnTree = false;
+								}
+							} else {
+								spawnTree = false;
+							}
+						}
+					}
+				}
+				if (!spawnTree) {
+					return false;
+				} else {
+					Block ground = world.getBlockState(position.add(0, -1, 0)).getBlock();
+					Block ground2 = world.getBlockState(position.add(0, -2, 0)).getBlock();
+					if (!((ground == DarkGrassBlockBlock.block.getDefaultState().getBlock()
+							|| ground == DarkDirtBlock.block.getDefaultState().getBlock())
+							&& (ground2 == DarkGrassBlockBlock.block.getDefaultState().getBlock()
+									|| ground2 == DarkDirtBlock.block.getDefaultState().getBlock())))
+						return false;
+					BlockState state = world.getBlockState(position.down());
+					if (position.getY() < world.getHeight() - height - 1) {
+						setTreeBlockState(changedBlocks, world, position.down(), DarkDirtBlock.block.getDefaultState(), bbox);
+						for (int genh = position.getY() - 3 + height; genh <= position.getY() + height; genh++) {
+							int i4 = genh - (position.getY() + height);
+							int j1 = (int) (1 - i4 * 0.5);
+							for (int k1 = position.getX() - j1; k1 <= position.getX() + j1; ++k1) {
+								for (int i2 = position.getZ() - j1; i2 <= position.getZ() + j1; ++i2) {
+									int j2 = i2 - position.getZ();
+									if (Math.abs(position.getX()) != j1 || Math.abs(j2) != j1 || rand.nextInt(2) != 0 && i4 != 0) {
+										BlockPos blockpos = new BlockPos(k1, genh, i2);
+										state = world.getBlockState(blockpos);
+										if (state.getBlock().isAir(state, world, blockpos) || state.getMaterial().blocksMovement()
+												|| state.isIn(BlockTags.LEAVES) || state.getBlock() == Blocks.VOID_AIR.getDefaultState().getBlock()
+												|| state.getBlock() == Blocks.OAK_LEAVES.getDefaultState().getBlock()) {
+											setTreeBlockState(changedBlocks, world, blockpos, Blocks.OAK_LEAVES.getDefaultState(), bbox);
+										}
+									}
+								}
+							}
+						}
+						for (int genh = 0; genh < height; genh++) {
+							BlockPos genhPos = position.up(genh);
+							state = world.getBlockState(genhPos);
+							setTreeBlockState(changedBlocks, world, genhPos, DarkOAKlogBlock.block.getDefaultState(), bbox);
+							if (state.getBlock().isAir(state, world, genhPos) || state.getMaterial().blocksMovement() || state.isIn(BlockTags.LEAVES)
+									|| state.getBlock() == Blocks.VOID_AIR.getDefaultState().getBlock()
+									|| state.getBlock() == Blocks.OAK_LEAVES.getDefaultState().getBlock()) {
+							}
+						}
+						if (rand.nextInt(4) == 0 && height > 5) {
+							for (int hlevel = 0; hlevel < 2; hlevel++) {
+								for (Direction Direction : Direction.Plane.HORIZONTAL) {
+									if (rand.nextInt(4 - hlevel) == 0) {
+										Direction dir = Direction.getOpposite();
+										setTreeBlockState(changedBlocks, world, position.add(dir.getXOffset(), height - 5 + hlevel, dir.getZOffset()),
+												Blocks.VOID_AIR.getDefaultState(), bbox);
+									}
+								}
+							}
+						}
+						return true;
+					} else {
+						return false;
+					}
+				}
+			} else {
+				return false;
+			}
+		}
+
+		private void addVines(IWorld world, BlockPos pos, Set<BlockPos> changedBlocks, MutableBoundingBox bbox) {
+			setTreeBlockState(changedBlocks, world, pos, Blocks.VOID_AIR.getDefaultState(), bbox);
+			int i = 5;
+			for (BlockPos blockpos = pos.down(); world.isAirBlock(blockpos) && i > 0; --i) {
+				setTreeBlockState(changedBlocks, world, blockpos, Blocks.VOID_AIR.getDefaultState(), bbox);
+				blockpos = blockpos.down();
+			}
+		}
+
+		private boolean canGrowInto(Block blockType) {
+			return blockType.getDefaultState().getMaterial() == Material.AIR || blockType == DarkOAKlogBlock.block.getDefaultState().getBlock()
+					|| blockType == Blocks.OAK_LEAVES.getDefaultState().getBlock()
+					|| blockType == DarkGrassBlockBlock.block.getDefaultState().getBlock()
+					|| blockType == DarkDirtBlock.block.getDefaultState().getBlock();
+		}
+
+		private boolean isReplaceable(IWorld world, BlockPos pos) {
+			BlockState state = world.getBlockState(pos);
+			return state.getBlock().isAir(state, world, pos) || canGrowInto(state.getBlock()) || !state.getMaterial().blocksMovement();
+		}
+
+		private void setTreeBlockState(Set<BlockPos> changedBlocks, IWorldWriter world, BlockPos pos, BlockState state, MutableBoundingBox mbb) {
+			super.func_227217_a_(world, pos, state, mbb);
+			changedBlocks.add(pos.toImmutable());
 		}
 	}
 }
